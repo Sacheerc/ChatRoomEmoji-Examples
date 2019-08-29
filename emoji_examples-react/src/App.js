935ae4c3b7906eca437d0c99b4b814824da99b7e
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-08-29 23:02:16 +0530

    Updated

--- emoji_examples-react/src/App.js
+++ emoji_examples-react/src/App.js
@@ -1,34 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div class="container p-5 align-items-center">
-    <h2 class="text-center">Emoji-Mart example component</h2>
-    <div class="container  m-5 border rounded m-5 p-4">
+import {
+  // [..]
+  addEmoji,
+  toggleEmojiPicker,
+} from './methods';
+import 'emoji-mart/css/emoji-mart.css';
 
-        
-        <div class="row mt-4 ">
-            <div class="col-md-10 input-group">              
-                <input class="form-control"> 
-                </input>
-                <button
-                      type="button"
-                      class="toggle-emoji"
-                            >
-                          </button>
+class App extends Component {
+  constructor() {
+    super();
+    this.addEmoji = addEmoji.bind(this);
+    this.toggleEmojiPicker = toggleEmojiPicker.bind(this);
+    this.state = {
+      showEmojiPicker: false
+    };
+  }
+
+  render() {
+    const {
+      showEmojiPicker,
+    } = this.state;
+    return (
+      <div className="App">
+        <div class="container p-5 align-items-center">
+          <h2 class="text-center">Emoji-Mart example component</h2>
+          <div class="container  m-5 border rounded m-5 p-4">
+            <div class="row mt-4 ">
+              <div class="col-md-10 input-group">
+                <input class="form-control"></input>
+                <button type="button" class="toggle-emoji"></button>
+                <button type="button" class="send-message ml-3"></button>
+              </div>
             </div>
-            
+          </div>
         </div>
-        
-    </div>
-   
-</div>
-</div>
-
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
